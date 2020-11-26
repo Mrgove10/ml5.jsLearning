6f227eeef1ec7f7b3b2aa121ed6ec44625c6ed35
Adrien RICHARD <richardadrien0@gmail.com>
2020-11-26 13:40:52 +0100

    cocossd and modelnet

---
+++ cocossd/webcam/sketch.js
@@ -0,0 +1,41 @@
+let video;
+let detector;
+let detections = [];
+function preload() {
+    detector = ml5.objectDetector('cocossd');
+}
+
+function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO)
+    video.size(640, 480)
+    video.hide()
+    detector.detect(video, gotdetector)
+}
+
+function draw() {
+    image(video, 0, 0);
+
+    detections.forEach(element => {
+        stroke(0, 255, 0);
+        strokeWeight(4);
+        noFill();
+        rect(element.x, element.y, element.width, element.height);
+        noStroke();
+        fill(255);
+        textSize(24);
+        text(element.label, element.x + 10, element.y + 24);
+    });
+    detector.detect(video, gotdetector)
+}
+
+function gotdetector(error, result) {
+    if (error) {
+        console.error(error)
+    }
+    detections = result;
+    detector.detect(video, gotdetector)
+}
+
+https://www.youtube.com/watch?v=QEzRxnuaZCk&list=PLRqwX-V7Uu6YPSwT06y_AEYTqIwbeam3y&index=5
+127.0.0.1:8080/
